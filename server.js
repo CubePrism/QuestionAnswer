ff54581ea41d6f7d31a8249645fa2d346e6026ea
Vaibhav Jatar <vaibhavjatar@gmail.com>
2014-10-02 12:57:42 +0000

    initial setup

--- server.js
+++ server.js
@@ -11,9 +11,15 @@ var path = require('path');
 var app = express();
 
 // all environments
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+//var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var server=http.createServer(app);
 app.set('port', process.env.PORT || 3000);
+app.set('ipaddress', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'jade');  
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -33,6 +39,7 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/carItHome', function(req, res) {     res.sendfile('./views/QandA.html'); });
 
-http.createServer(app).listen(app.get('port'), function(){
+
+server.listen(app.get('port'),app.get('ipaddress'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
